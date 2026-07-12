b6f9aec62f61bf132485875f05fff4c81fc0ce83
abot8639 <abot8639@gmail.com>
2026-07-13 00:08:21 +0300

    feat: add backward-compatible wrapper and remove nested git repository metadata

--- arduino-cli-manager.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# DEPRECATED: This script has been renamed to anodemcu.
-# Forwarding to the new script...
-echo -e "\033[0;33m[Warning] arduino-cli-manager.sh is deprecated. Please use anodemcu instead.\033[0m" >&2
-exec "$(dirname "$0")/anodemcu" "$@"
