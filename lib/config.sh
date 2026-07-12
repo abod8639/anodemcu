d644fc9daede4d892f369268b27698f9a82823cc
abot8639 <abot8639@gmail.com>
2026-07-12 23:11:07 +0300

    refactor: remove redundant package build files and consolidate project configuration logic

--- lib/config.sh
+++ lib/config.sh
@@ -22,12 +22,14 @@ LOG_FILE="$LOG_DIR/arduino-manager.log"
 
 # --- Colors ---
 C_RESET='\033[0m'
-C_RED='\033[0;31m'
-C_GREEN='\033[0;32m'
-C_YELLOW='\033[0;33m'
-C_BLUE='\033[0;34m'
-C_PURPLE='\033[0;35m'
-C_CYAN='\033[0;36m'
+C_RED='\033[38;2;243;139;168m'      # Catppuccin Red
+C_GREEN='\033[38;2;166;227;161m'    # Catppuccin Green
+C_YELLOW='\033[38;2;249;226;175m'   # Catppuccin Yellow
+C_BLUE='\033[38;2;137;180;250m'     # Catppuccin Blue
+C_PURPLE='\033[38;2;203;166;247m'   # Catppuccin Mauve
+C_CYAN='\033[38;2;137;220;235m'     # Catppuccin Sky
+C_LOGO='\033[38;2;203;166;247m'     # Catppuccin Mauve
+C_SHORTCUT='\033[38;2;180;190;254m' # Catppuccin Lavender
 
 # --- State Variables ---
 FQBN=""
